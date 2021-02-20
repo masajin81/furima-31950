5a1610503036ad73e631399682583e8fdcbb6dd4
masajin81 <masajin.0801@gmail.com>
2021-02-21 03:06:45 +0900

    ActiveHashを導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,20 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :delivery_fee
+  belongs_to :prefecture
+  belongs_to :shipping_day
+
   belongs_to :user
   has_one_attached :image
+
+  with_options, numericality: { other_than: 1 } do
+    validates :category
+    validates :condition
+    validates :delivery_fee
+    validates :prefecture
+    validates :shipping_day
+  end
+
 end
